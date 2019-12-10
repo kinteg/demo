42769a5ccf5639b8abda18fc04044fe58e222aff
kinteg <zeroxchao@gmail.com>
2019-12-10 19:29:04 +0300

    Create

---
+++ src/main/java/ru/iac/demo/controller/MainController.java
@@ -0,0 +1,19 @@
+package ru.iac.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+    @GetMapping
+    public String main(Model model) {
+        model.addAttribute("isDevMode", true);
+
+        return "index";
+    }
+
+}
